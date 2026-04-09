2703b8d051fadbf29e858977e3db23f4088f261d
gschumacher87-art <gschumacher87@gmail.com>
2026-04-09 21:56:11 +1000

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./api/auth");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Mechanix API running");
