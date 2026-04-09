1daeca103922c474fd002a9f7426806cfdd76e7e
gschumacher87-art <gschumacher87@gmail.com>
2026-04-09 21:43:50 +1000

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,14 @@
-console.log("Server running");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
