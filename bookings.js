e04bdc4b56ccc2b16cb726c6f8f47fce7e4af358
gschumacher87-art <gschumacher87@gmail.com>
2026-04-24 22:18:34 +1000

    Update bookings.js

---
+++ bookings.js
@@ -0,0 +1,25 @@
+let bookings = [];
+
+async function loadBookings() {
+    const res = await fetch(API + "/bookings");
+    bookings = await res.json();
+
+    let todayHtml = "";
+    const today = new Date().toLocaleDateString("en-CA");
+
+    bookings.forEach(b => {
+        const d = new Date(b.date).toLocaleDateString("en-CA");
+
+        if (d === today) {
+            todayHtml += `
+            <div class="card">
+                ${b.customer?.firstName || ""} ${b.customer?.lastName || ""}
+            </div>`;
+        }
+    });
+
+    document.getElementById("todayList").innerHTML =
+        todayHtml || "<div class='card'>No bookings</div>";
+}
+
+loadBookings();
