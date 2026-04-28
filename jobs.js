a34e69ba67ee3c1ab013592e94ee4a2ed236097a
gschumacher87-art <gschumacher87@gmail.com>
2026-04-28 15:39:12 +1000

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -27,11 +27,7 @@ async function loadJobs() {
         if (j.status === "completed") color = "green";
 
         const card = `
-<div class="card" onclick="${
-    j.status === 'pending-invoice'
-        ? `createInvoiceFromJob(\"${j._id}\")`
-        : `openJobCard(\"${j._id}\")`
-}" style="border-left:6px solid ${color}">
+<div class="card" onclick="handleJobClick('${j._id}', '${j.status}')" style="border-left:6px solid ${color}">
     <div class="title">${j.title}</div>
 <span class="status ${j.status}">${j.status}</span><br><br>
 
