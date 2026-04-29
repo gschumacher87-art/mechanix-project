e353671917bc86394c2771839c34baaca46bb8c3
gschumacher87-art <gschumacher87@gmail.com>
2026-04-29 18:57:45 +1000

    Create Part.js

---
+++ backend/src/models/Part.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const PartSchema = new mongoose.Schema({
+    category: { type: String, required: true },
+    name: { type: String, required: true },
+    partNumber: { type: String, required: true },
+    price: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Part", PartSchema);
