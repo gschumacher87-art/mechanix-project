5e98fdc3d7863b5277f3eb027ed4ee49c2dee7a8
gschumacher87-art <gschumacher87@gmail.com>
2026-04-30 18:10:41 +1000

    Update invoices.js

--- invoices.js
+++ invoices.js
@@ -117,7 +117,10 @@ let labourHtml = "";
 (template.items || []).forEach((i, index) => {
     itemsHtml += `
     <div style="display:flex; gap:6px; margin-bottom:6px;">
-        <input value="${i.partNumber || ""}" placeholder="Part Number" 
+        <input value="${i.category || ""}" placeholder="Category"
+    onchange="updateItem(${index}, 'category', this.value)">
+
+<input value="${i.partNumber || ""}" placeholder="Part Number"
     onchange="updateItem(${index}, 'partNumber', this.value)">
         <input type="number" value="${i.qty || 1}" style="width:60px"
             onchange="updateItem(${index}, 'qty', this.value)">
@@ -455,11 +458,14 @@ function addItem() {
 
     const index = currentInvoice.template.items.length;
 
-    currentInvoice.template.items.push({ partNumber: "", qty: 1, price: 0 });
+    currentInvoice.template.items.push({ category: "", partNumber: "", qty: 1, price: 0 });
 
     const row = `
     <div style="display:flex; gap:6px; margin-bottom:6px;">
-        <input placeholder="Part Number"
+        <input placeholder="Category"
+    onchange="updateItem(${index}, 'category', this.value)">
+
+<input placeholder="Part Number"
     onchange="updateItem(${index}, 'partNumber', this.value)">
 
         <input type="number" value="1" style="width:60px"
