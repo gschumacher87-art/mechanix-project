924877f221dd52cffa589cecd4327336711408f9
gschumacher87-art <gschumacher87@gmail.com>
2026-05-31 13:35:43 +1000

    Update invoices.js

--- invoices.js
+++ invoices.js
@@ -17,6 +17,8 @@ async function loadInvoices() {
 
     const pendingJobs = jobsData.filter(j => j.status === "pending-invoice");
 
+alert(JSON.stringify(pendingJobs[0]));
+
     const invoiceCards = invoices.map(i => {
 
         const template = i.template || { items: [], labour: [] };
