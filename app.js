18f045965244255294f4f35c1fb95915804f7f42
gschumacher87-art <gschumacher87@gmail.com>
2026-08-06 20:07:59 +1000

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ window.fetch = function(url, options = {}) {
 
     return originalFetch(url, options).then(res => {
     if (res.status === 401) {
-    alert("API RETURNED 401");
     localStorage.removeItem("token");
     location.reload();
     return;
