33da3784292a851fa99a808fab0dd806db6e54e0
gschumacher87-art <gschumacher87@gmail.com>
2026-08-23 16:00:29 +1000

    Update Part.js

--- backend/src/models/Part.js
+++ backend/src/models/Part.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const PartSchema = new mongoose.Schema({
     category: { type: String, required: true },
+    description: { type: String, required: true },
     partNumber: { type: String, required: true },
     price: { type: Number, default: 0 }
 }, { timestamps: true });
-
 module.exports = mongoose.model("Part", PartSchema);
